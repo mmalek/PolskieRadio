3eaa89875b092d7de31dbdb0e1829835f12bca46
Michal Malek <michalm@jabster.pl>
2012-02-14 22:12:57 +0100

    Show channels' categories in the main menu (+ Pocasts menu)

--- PolskieRadioApplet.lua
+++ PolskieRadioApplet.lua
@@ -9,36 +9,26 @@
 -------------------------------------------------------------------------------
 
 -- stuff we use
-local assert, getmetatable, ipairs, pcall, setmetatable, tonumber, tostring = assert, getmetatable, ipairs, pcall, setmetatable, tonumber, tostring
+local assert, getmetatable, ipairs, pairs, pcall, setmetatable, tonumber, tostring = assert, getmetatable, ipairs, pairs, pcall, setmetatable, tonumber, tostring
 
 local oo                     = require("loop.simple")
 
-local string                 = require("string")
-local table                  = require("jive.utils.table")
-local io                     = require("io")
-local SocketHttp             = require("jive.net.SocketHttp")
-local RequestHttp            = require("jive.net.RequestHttp")
 local json                   = require("json")
-local math                   = require("math")
 
 local Applet                 = require("jive.Applet")
-local System                 = require("jive.System")
+local SocketHttp             = require("jive.net.SocketHttp")
+local RequestHttp            = require("jive.net.RequestHttp")
 local Framework              = require("jive.ui.Framework")
 local Icon                   = require("jive.ui.Icon")
-local Label                  = require("jive.ui.Label")
-local Textarea               = require("jive.ui.Textarea")
 local Window                 = require("jive.ui.Window")
-local Popup                  = require("jive.ui.Popup")
 local SimpleMenu             = require("jive.ui.SimpleMenu")
 local Timer                  = require("jive.ui.Timer")
-local Player                 = require("jive.slim.Player")
-local jnt                    = jnt
-
-local JIVE_VERSION           = jive.JIVE_VERSION
-
 local debug                  = require("jive.utils.debug")
+local Player                 = require("jive.slim.Player")
+
 local appletManager          = appletManager
 local jiveMain               = jiveMain
+local jnt                    = jnt
 
 local CHANNELS_HOST = "moje.polskieradio.pl"
 local CHANNELS_QUERY = "api/?key=20439fdf-be66-4852-9ded-1476873cfa22&output=json"
@@ -78,12 +68,9 @@ module(..., Framework.constants)
 oo.class(_M, Applet)
 
 function show(self, menuItem)
-	
+
 	local window = Window("text_list", menuItem.text)
 	local menu = SimpleMenu("menu")
-	--window:addWidget(menu)
-	--local textArea = Textarea("text", "Jakistam tekst", "\n")
-	--window:addWidget(textArea)
 	window:addWidget(menu)
 
 	self:tieAndShowWindow(window)
@@ -97,12 +84,14 @@ function show(self, menuItem)
 	timer:start()
 	log:info("timer start")
 
+
 	local function sink(chunk, err)
 		if err then
 			log:error( err )
 		elseif chunk then
-			self.channels = json.decode(chunk).channel
- 			for i,channel in ipairs(self.channels) do
+			local cat2Menus = {}
+			local channels = json.decode(chunk).channel
+ 			for i,channel in ipairs(channels) do
 				--log:info("received JSON element: ", )
 				local newMenuItem = {
 					id = channel.id,
@@ -118,17 +107,53 @@ function show(self, menuItem)
 					local uri = req:getURI()
 					local http = SocketHttp(jnt, uri.host, uri.port, uri.host)
 					http:fetch(req)
-					menu:addItem(newMenuItem)
+
+					
+					local menuItems = cat2Menus[channel.category]
+					if menuItems == nil then
+						menuItems = {}
+					end
+					menuItems[#menuItems + 1] = newMenuItem
+					cat2Menus[channel.category] = menuItems
 				end
  			end
+
+			for category,subItems in pairs(cat2Menus) do
+				menu:addItem( { text = category, callback = function(event,menuItem) self:showCategoryItems(menuItem,subItems) end } )
+			end
+
+			menu:addItem( { text = self:string("PODCASTS"), callback = function(event,menuItem) self:showPodcasts( menuItem ) end } )
 		end
 	end
-	
+
 	local http = SocketHttp(jnt, CHANNELS_HOST, 80)
 	local req = RequestHttp(sink, 'GET', 'http://' .. CHANNELS_HOST .. '/' .. CHANNELS_QUERY )
 
 	-- go get it!
 	http:fetch(req)
 	
+	return window
+end
+
+function showCategoryItems(self, menuItem, subItems)
+
+	local window = Window("text_list", menuItem.text)
+	local menu = SimpleMenu("menu")
+	menu:setItems(subItems)
+	window:addWidget(menu)
+
+	self:tieAndShowWindow(window)
+
+	return window
+end
+
+function showPodcasts(self, menuItem)
+
+	local window = Window("text_list", menuItem.text)
+	local menu = SimpleMenu("menu")
+	window:addWidget(menu)
+
+	self:tieAndShowWindow(window)
+
 	return window
 end
