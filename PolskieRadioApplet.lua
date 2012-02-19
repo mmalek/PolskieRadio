337ce4b058682f767d8d04e9653cbddcf10af6d8
Michal Malek <michalm@jabster.pl>
2012-02-19 10:26:11 +0100

    Lock menus when loading stuff from the internet

--- PolskieRadioApplet.lua
+++ PolskieRadioApplet.lua
@@ -23,7 +23,6 @@ local Framework              = require("jive.ui.Framework")
 local Icon                   = require("jive.ui.Icon")
 local Window                 = require("jive.ui.Window")
 local SimpleMenu             = require("jive.ui.SimpleMenu")
-local Timer                  = require("jive.ui.Timer")
 local debug                  = require("jive.utils.debug")
 local Player                 = require("jive.slim.Player")
 
@@ -33,45 +32,43 @@ local jnt                    = jnt
 
 local CHANNELS_HOST = "moje.polskieradio.pl"
 local CHANNELS_QUERY = "api/?key=20439fdf-be66-4852-9ded-1476873cfa22&output=json"
-local PODCAST_HOST = "www.polskieradio.pl"
-local PODCAST_BASE = "Podcast"
 local PODCAST_FEEDS = {
-	{ text = "Bardzo Ważny Projekt",                                feed = "fe75a17b-3b54-4c70-a639-b332dfbac3a4" },
-	{ text = "Biegam, bo lubię",                                    feed = "5757e01b-0754-4598-a74e-9059285223c1" },
-	{ text = "Co w mowie piszczy",                                  feed = "c9051f3f-e5eb-4e39-82e9-a943e017536d" },
-	{ text = "Dobronocka",                                          feed = "da0d7581-4fb7-446d-8d55-b778f10d0830" },
-	{ text = "Europejski Magazyn Kulturalny",                       feed = "9635535f-13d0-42f5-a895-8c5e7a0f726a" },
-	{ text = "Fajny film wczoraj widziałem",                        feed = "ab3dd76e-fcef-4446-b352-13fe6e75e007" },
-	{ text = "Głosy z przeszłości",                                 feed = "11037aae-a0f3-4a2e-8853-17fa1d12c3f6" },
-	{ text = "Instukcja obsługi człowieka",                         feed = "894e36d3-17e5-48e2-bd83-aad181c622ca" },
-	{ text = "Jest taki obraz",                                     feed = "c1383df8-8582-40d6-90a3-bd30029f3607" },
-	{ text = "Klub Ludzi Ciekawych Wszystkiego",                    feed = "2c77df19-395a-427c-b8f2-fa2fbf3f9eac" },
-	{ text = "Klub Trójki",                                         feed = "ed6dc47e-7c5c-4af0-9ce5-f17272510f0d" },
-	{ text = "Kulturalny Wieczór z Jedynką",                        feed = "ce516fd0-c774-46db-940c-35ea279303b8" },
-	{ text = "Lista osobista",                                      feed = "8875a187-1d5b-479e-b762-e0b514a6e928" },
-	{ text = "Na wyciągnięcie ręki",                                feed = "ff1444d6-e6e2-4d9a-b3bd-e8cd4b3e5af2" },
-	{ text = "Notatnik Dwójki",                                     feed = "8c1328f7-d549-4e70-9691-e1804cc74df8" },
-	{ text = "Puls Trójki",                                         feed = "7a5bd819-fce6-47af-a42d-06ab8f65043b" },
-	{ text = "Raport o stanie świata",                              feed = "02ba1fbb-a0b3-436c-a517-931af8609368" },
-	{ text = "Reportaż",                                            feed = "7bf37c2d-6975-4311-9707-d9c1031adf4c" },
-	{ text = "Salon ekonomiczny Trójki i Dziennika Gazety Prawnej", feed = "d731f91b-1f1f-4f62-bcbe-e89d7a5ed50f" },
-	{ text = "Salon polityczny Trójki",                             feed = "6e8788c0-1d6e-4cce-b04b-765a80ac52e0" },
-	{ text = "Sezon na Dwójkę",                                     feed = "1ca42926-71ac-483d-8f94-a125305f537f" },
-	{ text = "Słowa po zmroku",                                     feed = "ae1e4ae1-1f38-418b-b842-535ffcc43f60" },
-	{ text = "Słynne powieści",                                     feed = "e64c2b0b-2c48-4483-a935-8b7dd408802a" },
-	{ text = "Śniadanie w Trójce",                                  feed = "0ace19e8-ed65-4232-9f43-affc040ca14d" },
-	{ text = "Sygnały dnia",                                        feed = "f8204f32-8522-4ae1-b081-284cc7b95508" },
-	{ text = "Teren Kultura",                                       feed = "2ef02f5f-e604-49f3-9029-76e80ea7aded" },
-	{ text = "Trójkowo, filmowo",                                   feed = "b5b30a95-662b-4554-be99-fa3f43d54c20" },
-	{ text = "Tygodnik literacki",                                  feed = "aea2a083-26aa-4af7-8286-fb0f53b4c57a" },
-	{ text = "Tym żył świat",                                       feed = "6a681240-327a-46e2-9eff-3339ecf8b3a5" },
-	{ text = "W stronę sztuki",                                     feed = "dcfa48f3-7c0a-4acd-b8ad-1777df031c44" },
-	{ text = "Za, a nawet przeciw",                                 feed = "c2a8a112-3d6a-41b4-b253-e96a0e585eb9" },
-	{ text = "Zagadkowa niedziela",                                 feed = "ed4ef025-a23f-4746-a458-9fb51b158425" },
-	{ text = "Zakładka literacka",                                  feed = "14017c7c-44f7-4241-8001-d1819af26638" },
-	{ text = "Zapiski ze współczesności",                           feed = "8579fd71-b09e-4274-8bd8-c93efe7fb4ac" },
-	{ text = "Zapraszamy do Trójki - popołudnie",                   feed = "ea372692-7321-4876-a5f8-38cd39f141db" },
-	{ text = "Zapraszamy do Trójki - ranek",                        feed = "c9b49e69-cb4c-48e4-aca5-98c574981e2f" }
+	{ text = "Bardzo Ważny Projekt",                                host = "www.polskieradio.pl",  query = "Podcast/fe75a17b-3b54-4c70-a639-b332dfbac3a4" },
+	{ text = "Biegam, bo lubię",                                    host = "www.polskieradio.pl",  query = "Podcast/5757e01b-0754-4598-a74e-9059285223c1" },
+	{ text = "Co w mowie piszczy",                                  host = "www.polskieradio.pl",  query = "Podcast/c9051f3f-e5eb-4e39-82e9-a943e017536d" },
+	{ text = "Dobronocka",                                          host = "www.polskieradio.pl",  query = "Podcast/da0d7581-4fb7-446d-8d55-b778f10d0830" },
+	{ text = "Europejski Magazyn Kulturalny",                       host = "www2.polskieradio.pl", query = "podcast/102/podcast.xml" },
+	{ text = "Fajny film wczoraj widziałem",                        host = "www.polskieradio.pl",  query = "Podcast/ab3dd76e-fcef-4446-b352-13fe6e75e007" },
+	{ text = "Głosy z przeszłości",                                 host = "www.polskieradio.pl",  query = "Podcast/11037aae-a0f3-4a2e-8853-17fa1d12c3f6" },
+	{ text = "Instukcja obsługi człowieka",                         host = "www.polskieradio.pl",  query = "Podcast/894e36d3-17e5-48e2-bd83-aad181c622ca" },
+	{ text = "Jest taki obraz",                                     host = "www.polskieradio.pl",  query = "Podcast/c1383df8-8582-40d6-90a3-bd30029f3607" },
+	{ text = "Klub Ludzi Ciekawych Wszystkiego",                    host = "www.polskieradio.pl",  query = "Podcast/2c77df19-395a-427c-b8f2-fa2fbf3f9eac" },
+	{ text = "Klub Trójki",                                         host = "www.polskieradio.pl",  query = "Podcast/ed6dc47e-7c5c-4af0-9ce5-f17272510f0d" },
+	{ text = "Kulturalny Wieczór z Jedynką",                        host = "www.polskieradio.pl",  query = "Podcast/ce516fd0-c774-46db-940c-35ea279303b8" },
+	{ text = "Lista osobista",                                      host = "www.polskieradio.pl",  query = "Podcast/8875a187-1d5b-479e-b762-e0b514a6e928" },
+	{ text = "Na wyciągnięcie ręki",                                host = "www.polskieradio.pl",  query = "Podcast/ff1444d6-e6e2-4d9a-b3bd-e8cd4b3e5af2" },
+	{ text = "Notatnik Dwójki",                                     host = "www.polskieradio.pl",  query = "Podcast/8c1328f7-d549-4e70-9691-e1804cc74df8" },
+	{ text = "Puls Trójki",                                         host = "www.polskieradio.pl",  query = "Podcast/7a5bd819-fce6-47af-a42d-06ab8f65043b" },
+	{ text = "Raport o stanie świata",                              host = "www.polskieradio.pl",  query = "Podcast/02ba1fbb-a0b3-436c-a517-931af8609368" },
+	{ text = "Reportaż",                                            host = "www.polskieradio.pl",  query = "Podcast/7bf37c2d-6975-4311-9707-d9c1031adf4c" },
+	{ text = "Salon ekonomiczny Trójki i Dziennika Gazety Prawnej", host = "www.polskieradio.pl",  query = "Podcast/d731f91b-1f1f-4f62-bcbe-e89d7a5ed50f" },
+	{ text = "Salon polityczny Trójki",                             host = "www.polskieradio.pl",  query = "Podcast/6e8788c0-1d6e-4cce-b04b-765a80ac52e0" },
+	{ text = "Sezon na Dwójkę",                                     host = "www.polskieradio.pl",  query = "Podcast/1ca42926-71ac-483d-8f94-a125305f537f" },
+	{ text = "Słowa po zmroku",                                     host = "www.polskieradio.pl",  query = "Podcast/ae1e4ae1-1f38-418b-b842-535ffcc43f60" },
+	{ text = "Słynne powieści",                                     host = "www.polskieradio.pl",  query = "Podcast/e64c2b0b-2c48-4483-a935-8b7dd408802a" },
+	{ text = "Śniadanie w Trójce",                                  host = "www.polskieradio.pl",  query = "Podcast/0ace19e8-ed65-4232-9f43-affc040ca14d" },
+	{ text = "Sygnały dnia",                                        host = "www.polskieradio.pl",  query = "Podcast/f8204f32-8522-4ae1-b081-284cc7b95508" },
+	{ text = "Teren Kultura",                                       host = "www.polskieradio.pl",  query = "Podcast/2ef02f5f-e604-49f3-9029-76e80ea7aded" },
+	{ text = "Trójkowo, filmowo",                                   host = "www.polskieradio.pl",  query = "Podcast/b5b30a95-662b-4554-be99-fa3f43d54c20" },
+	{ text = "Tygodnik literacki",                                  host = "www.polskieradio.pl",  query = "Podcast/aea2a083-26aa-4af7-8286-fb0f53b4c57a" },
+	{ text = "Tym żył świat",                                       host = "www.polskieradio.pl",  query = "Podcast/6a681240-327a-46e2-9eff-3339ecf8b3a5" },
+	{ text = "W stronę sztuki",                                     host = "www.polskieradio.pl",  query = "Podcast/dcfa48f3-7c0a-4acd-b8ad-1777df031c44" },
+	{ text = "Za, a nawet przeciw",                                 host = "www.polskieradio.pl",  query = "Podcast/c2a8a112-3d6a-41b4-b253-e96a0e585eb9" },
+	{ text = "Zagadkowa niedziela",                                 host = "www.polskieradio.pl",  query = "Podcast/ed4ef025-a23f-4746-a458-9fb51b158425" },
+	{ text = "Zakładka literacka",                                  host = "www.polskieradio.pl",  query = "Podcast/14017c7c-44f7-4241-8001-d1819af26638" },
+	{ text = "Zapiski ze współczesności",                           host = "www.polskieradio.pl",  query = "Podcast/8579fd71-b09e-4274-8bd8-c93efe7fb4ac" },
+	{ text = "Zapraszamy do Trójki - popołudnie",                   host = "www.polskieradio.pl",  query = "Podcast/ea372692-7321-4876-a5f8-38cd39f141db" },
+	{ text = "Zapraszamy do Trójki - ranek",                        host = "www.polskieradio.pl",  query = "Podcast/c9b49e69-cb4c-48e4-aca5-98c574981e2f" }
 }
 
 local _streamCallback = function(event, menuItem)
@@ -110,24 +107,15 @@ oo.class(_M, Applet)
 
 function show(self, menuItem)
 
+	local previousMenu = jiveMain:getNodeMenu( menuItem.node )
 	local window = Window("text_list", menuItem.text)
 	local menu = SimpleMenu("menu")
 	window:addWidget(menu)
 
-	self:tieAndShowWindow(window)
-
-	--menu:lock()
-	local timer = Timer(1000,
-		function()
-			log:info("timer end")
-			--window:show()
-		end, true)
-	timer:start()
-	log:info("timer start")
-
 	local function sink(chunk, err)
 		if err then
 			log:error( err )
+			previousMenu:unlock()
 		elseif chunk then
 			local cat2Menus = {}
 			local channels = json.decode(chunk).channel
@@ -148,7 +136,6 @@ function show(self, menuItem)
 					local http = SocketHttp(jnt, uri.host, uri.port, uri.host)
 					http:fetch(req)
 
-					
 					local menuItems = cat2Menus[channel.category]
 					if menuItems == nil then
 						menuItems = {}
@@ -163,16 +150,19 @@ function show(self, menuItem)
 			end
 
 			menu:addItem( { text = self:string("PODCASTS"), callback = function(event,menuItem) self:showPodcastList( menuItem ) end } )
+			self:tieAndShowWindow(window)
+			previousMenu:unlock()
 		end
 	end
 
 	local http = SocketHttp(jnt, CHANNELS_HOST, 80)
 	local req = RequestHttp(sink, 'GET', 'http://' .. CHANNELS_HOST .. '/' .. CHANNELS_QUERY )
 
+	-- lock the previous menu till we load the RSS file
+	previousMenu:lock( function() http:close() end )
+
 	-- go get it!
 	http:fetch(req)
-	
-	return window
 end
 
 function showCategoryItems(self, menuItem, subItems)
@@ -195,14 +185,14 @@ function showPodcastList(self, menuItem)
 	window:addWidget(menu)
 
 	for _,feed in pairs(PODCAST_FEEDS) do
-		menu:addItem( { text = feed.text, callback = function(event,menuItem) self:showPodcastContent(menuItem, feed.feed) end } )
+		menu:addItem( { text = feed.text, callback = function(event,menuItem) self:showPodcastContent(menu, menuItem, feed) end } )
 	end
 
 	self:tieAndShowWindow(window)
 	return window
 end
 
-function showPodcastContent(self, menuItem, feed)
+function showPodcastContent(self, previousMenu, menuItem, feed)
 
 	local window = Window("text_list", menuItem.text)
 	local menu = SimpleMenu("menu")
@@ -211,6 +201,7 @@ function showPodcastContent(self, menuItem, feed)
 	local function sink(chunk, err)
 		if err then
 			log:error( err )
+			previousMenu:unlock()
 		elseif chunk then
 			local rss = lom.parse( chunk )
 			for _, entry in ipairs(rss) do
@@ -237,15 +228,17 @@ function showPodcastContent(self, menuItem, feed)
 					end
 				end
 			end
+			previousMenu:unlock()
+			self:tieAndShowWindow(window)
 		end
 	end
 
-	local http = SocketHttp(jnt, PODCAST_HOST, 80)
-	local req = RequestHttp(sink, 'GET', 'http://' .. PODCAST_HOST .. '/' .. PODCAST_BASE .. '/' .. feed )
+	local http = SocketHttp(jnt, feed.host, 80)
+	local req = RequestHttp(sink, 'GET', 'http://' .. feed.host .. '/' .. feed.query )
+
+	-- lock the previous menu till we load the RSS file
+	previousMenu:lock( function() http:close() end )
 
 	-- go get it!
 	http:fetch(req)
-
-	self:tieAndShowWindow(window)
-	return window
 end
